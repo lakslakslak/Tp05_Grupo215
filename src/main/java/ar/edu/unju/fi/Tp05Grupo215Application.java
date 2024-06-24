97b0bcabad6be13e30cd3d8e7f32ba8b45742618
User <User@DESKTOP-P5FD361>
2024-06-23 22:10:07 -0300

    Inicializacion del Proyecto. Vladimir Cachi

---
+++ src/main/java/ar/edu/unju/fi/Tp05Grupo215Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp05Grupo215Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp05Grupo215Application.class, args);
+	}
+
+}
